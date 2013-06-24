7caf3ea3e2c7b4c2d1d2d3b3ae2e7aa0238147d0
才国 <caiguo@saikokutekiMac.local>
2013-06-24 07:43:39 -0700

    sdfsdfs

---
+++ uih/uih/testCell.h
@@ -0,0 +1,13 @@
+//
+//  testCell.h
+//  uih
+//
+//  Created by 才国 on 13-6-24.
+//  Copyright (c) 2013年 才国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testCell : UITableViewCell
+
+@end
---
+++ uih/uih/testCell.m
@@ -0,0 +1,29 @@
+//
+//  testCell.m
+//  uih
+//
+//  Created by 才国 on 13-6-24.
+//  Copyright (c) 2013年 才国. All rights reserved.
+//
+
+#import "testCell.h"
+
+@implementation testCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

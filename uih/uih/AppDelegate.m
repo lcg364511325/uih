dcb4ab372f8e063386e7563476f8303bd3e16f29
yjsmyhome <yjsmyhome@163.com>
2013-06-27 00:05:01 +0800

    hh

--- uih/uih/AppDelegate.m
+++ uih/uih/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"");
     // Override point for customization after application launch.
     return YES;
 }
